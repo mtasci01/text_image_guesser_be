1de0a244d974e9c5c0c6c6318fa87b3b764f1f29
Marco Tascini <tascinimarco@tiscali.it>
2024-07-28 13:05:04 +0200

    img fast guess

--- image_fast_guesser.py
+++ image_fast_guesser.py
@@ -3,8 +3,9 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+from PIL import Image
 
-foldername = "C:/Users/XYZ/Downloads/guided_tours/guided_tours"
+foldername = "E:/image_game"
 files = os.listdir(foldername)
 files_list = []
 for file in files:
@@ -12,7 +13,9 @@ for file in files:
 
 chosen = np.random.choice(files_list)
 
-img = mpimg.imread(foldername + "/" + chosen)
-imgplot = plt.imshow(img)
-plt.show()
+# img = mpimg.imread(foldername + "/" + chosen)
+# imgplot = plt.imshow(img)
+# plt.show()
+img = Image.open(foldername + "/" + chosen)
+img.show()
 print(chosen)
